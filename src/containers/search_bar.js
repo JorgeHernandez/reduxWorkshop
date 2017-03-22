aea56e545fcbe63bf1c994b10623879093846bcf
Jorge Hernández <diego.hernandez@i3television.es>
2017-03-22 17:10:23 +0100

    step 3 - SearchBar refinement (avoid submit)

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -7,12 +7,15 @@ export default class SearchBar extends Component {
 		this.onInputChange = this.onInputChange.bind(this);//Otherwise, this.setState will fail
 	}
 	onInputChange(event){
-		console.log(event.target.value);
 		this.setState({term:event.target.value});
+	}
+	onFormSubmit(event){
+		event.preventDefault();
+		//We need to go and fetch weather data here!!
 	}
 	render() {
 		return (
-			<form className="input-group">
+			<form onSubmit={this.onFormSubmit} className="input-group">
 				<input 
 					placeholder="Get a five day forecast in your favorite cities"
 					className="form-control" 
