79bf6d9273a0ddcd41250dd7cea88f92eefecd7b
Jorge Hernández <diego.hernandez@i3television.es>
2017-03-23 17:33:46 +0100

    step 6 - added axios - ajax call

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,18 @@
+import axios from 'axios';
+
 const API_KEY = '25766aff9cb7d9c86e9effb624feeddb';
+
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city){
+	const url = `$(ROOT_URL)&q=$(city),us`;
+
+	const request = axios.get(url);
+
+	return {
+		type: FETCH_WEATHER,
+		payload: request
+	}
+}
