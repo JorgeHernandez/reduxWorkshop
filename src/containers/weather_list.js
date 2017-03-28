b13c26c14620cae40d8a4ed1470be10e19a1c456
Jorge Hernández <diego.hernandez@i3television.es>
2017-03-28 19:09:36 +0200

    step 10 - basic result list

---
+++ src/containers/weather_list.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class WeatherList extends Component {
+	render() {
+		return (
+			<table className="table table-hover">
+				<thead>
+					<th>City</th>
+				</thead>
+				<tbody>
+				</tbody>
+			</table>
+		);
+	}
+}
